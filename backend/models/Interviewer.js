f173a7c5d5906440b051a0143fb48c23f95b0892
Muhammad Balach <muhammadbalach369@gmail.com>
2026-02-03 14:06:12 +0500

    Caches removed

--- backend/models/Interviewer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const interviewerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  expertise_field: { type: String },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Interviewer", interviewerSchema);
